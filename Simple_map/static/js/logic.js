fb7dc31300a2e03b7746891365bdbe263e56bbae
guevarch <107594143+guevarch@users.noreply.github.com>
2022-09-09 15:35:22 -0400

    files to add

--- Simple_map/static/js/logic.js
+++ Simple_map/static/js/logic.js
@@ -18,3 +18,14 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
+  // Loop through the cities array and create one marker for each city.
+  // Get data from cities.js
+let cityData = cities;
+  // Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+  console.log(city)
+  L.circleMarker(city.location, {radius: city.population/100000})
+  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population + "</h3>")
+.addTo(map);
+});
+  
