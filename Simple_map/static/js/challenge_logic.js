b91d5e2a7dea2e4ae4f8508c167e2248153f7473
guevarch <107594143+guevarch@users.noreply.github.com>
2022-09-11 17:28:49 -0400

    adding files

--- Simple_map/static/js/challenge_logic.js
+++ Simple_map/static/js/challenge_logic.js
@@ -31,11 +31,13 @@ let baseMaps = {
 // 1. Add a 2nd layer group for the tectonic plate data.
 let allEarthquakes = new L.LayerGroup();
 let Tectonic = new L.LayerGroup();
+let MajorEarthquakes = new L.LayerGroup();
 
 // 2. Add a reference to the tectonic plates group to the overlays object.
 let overlays = {
   "Earthquakes": allEarthquakes,
   "Tectonic": Tectonic,
+  "Major Earthquake": MajorEarthquakes,
 };
 
 // Then we add a control to the map that will allow the user to change which
@@ -143,13 +145,72 @@ legend.onAdd = function() {
   d3.json(tecdata).then(function(data) {
   console.log(data);
   L.geoJSON(data, {
-    color: '#3388ff',
+    color: '#ea822c',
     weight: 5
   }).addTo(Tectonic);
 
   // Then we add the earthquake layer to our map.
   Tectonic.addTo(map);
   });
+
+  d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson").then(function(data) {
+
+    // This function returns the style data for each of the earthquakes we plot on
+    // the map. We pass the magnitude of the earthquake into two separate functions
+    // to calculate the color and radius.
+    function styleInfo(feature) {
+      return {
+        opacity: 1,
+        fillOpacity: 1,
+        fillColor: getColor(feature.properties.mag),
+        color: "#000000",
+        radius: getRadius(feature.properties.mag),
+        stroke: true,
+        weight: 0.5
+      };
+    }
   
+    // This function determines the color of the marker based on the magnitude of the earthquake.
+    function getColor(magnitude) {
+      if (magnitude > 6) {
+        return "#ea2c2c";
+      }
+      if (magnitude >= 5) {
+        return "#ea822c";
+      }
+      if (magnitude < 5) {
+        return "#ee9c00";
+      }
+      
+    }
+  
+    // This function determines the radius of the earthquake marker based on its magnitude.
+    // Earthquakes with a magnitude of 0 were being plotted with the wrong radius.
+    function getRadius(magnitude) {
+      if (magnitude === 0) {
+        return 1;
+      }
+      return magnitude * 8;
+    }
+  
+    // Creating a GeoJSON layer with the retrieved data.
+    L.geoJson(data, {
+        // We turn each feature into a circleMarker on the map.
+        pointToLayer: function(feature, latlng) {
+            console.log(data);
+            return L.circleMarker(latlng);
+          },
+        // We set the style for each circleMarker using our styleInfo function.
+      style: styleInfo,
+       // We create a popup for each circleMarker to display the magnitude and location of the earthquake
+       //  after the marker has been created and styled.
+       onEachFeature: function(feature, layer) {
+        layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+      }
+    }).addTo(MajorEarthquakes);
+  
+    // Then we add the earthquake layer to our map.
+    MajorEarthquakes.addTo(map);
+  });
 });
   
